5abb50ebeba58dab4661d471068e300370d4bdb2
Mahdi Falamarzi <mehdi.kindly@gmail.com>
2019-12-27 16:47:07 +0330

    change package.json

--- src/components/Weather.jsx
+++ src/components/Weather.jsx
@@ -25,18 +25,20 @@ export default function Weather({ weather }) {
                                     </div>
                                     <div className="card-body">
                                         <h5 className="card-title">
-                                            City: {
+                                            City:
+                                            {
                                                 item.cod === "404"
                                                     ? 'Not Found!'
                                                     : item.name
                                             }
                                         </h5>
                                         <ul className="list-group list-group-flush">
-                                            <li className="list-group-item">Country: {
-                                                item.cod === "404"
-                                                    ? "Not Found!"
-                                                    : item.sys.country
-                                            }</li>
+                                            <li className="list-group-item">Country:
+                                            {
+                                                    item.cod === "404"
+                                                        ? "Not Found!"
+                                                        : item.sys.country
+                                                }</li>
                                             <li className="list-group-item">Humidity: {
                                                 item.cod === "404"
                                                     ? "Not Found!"
